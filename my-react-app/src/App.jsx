b7d372fe1fb16ec30cee4b1f3a700ee5ae37de71
Candraww <candrawijaya.skw5@gmail.com>
2025-11-19 00:10:40 +0700

    HomePart1

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,10 +4,17 @@ import Language from "./component/Language.jsx"
 import Card from "./component/Card.jsx"
 import githubpic from "./assets/github.png"
 import images from "./assets/image.png"
+import CardParent from "./component/CardParent.jsx"
+
 function App() {
   return (
     <>
     <Header/>
+    <CardParent 
+      ltitle="git" ldesc="git is a distributed version control system."
+      rtitle="git" rdesc="git is a distributed version control system."
+      
+    />
     <Card title ="git" desc="git is a distributed version control system."
      image={githubpic}
     />
--- my-react-app/src/component/CardParent.jsx
+++ my-react-app/src/component/CardParent.jsx
@@ -1,16 +1,43 @@
-
-function CardParent() {
+import styles from './Component.module.css'
+import githubpic from "../assets/github.png"
+import Card from "./Card.jsx"
+function CardParent(props) {
     return (
-        <div>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className={styles.homeCard}>
+            <div className={styles.leftSide}>
+                <div className={styles.leftCard}>
+                    <img className={styles.pcardImage} src={props.pimage} alt={props.ptitle} />
+                                
+                    <div className={styles.ptxt}>
+                        <h2 className={styles.cardTitle}>{props.ptitle}</h2>
+                        <p className={styles.cardText}>{props.pdesc}</p> 
+                    </div>
+                </div>
+
+            </div>
+                
+            <div className={styles.rightSide}>
+                <div className={styles.rightCard}>
+                    <div className={styles.firstRcard}>
+                        <img className={styles.rcardImage} src={props.limage} alt={props.ltitle} />
+                                
+                        <div className={styles.rtxt}>
+                            <h2 className={styles.rcardTitle}>{props.ltitle}</h2>
+                            <p className={styles.rcardText}>{props.ldesc}</p> 
+                        </div>    
+                    </div>
+
+                    <div className={styles.secondRcard}>
+                        <img className={styles.rcardImage} src={props.rimage} alt={props.rtitle} />
+                                
+                        <div className={styles.rtxt}>
+                            <h2 className={styles.rcardTitle}>{props.rtitle}</h2>
+                            <p className={styles.rcardText}>{props.rdesc}</p> 
+                        </div>    
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }   
